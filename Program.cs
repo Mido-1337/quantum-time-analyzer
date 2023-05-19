cabc3983e72008ef73f906285cda1fbd9a9c194b
Mido <133521799+Mido-1337@users.noreply.github.com>
2023-05-19 21:19:02 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Numerics;
+
+public class QuantumTimeAnalyzer
+{
+    public static void Main()
+    {
+        // Prompt the user for input
+        Console.Write("Enter the number of particles: ");
+        int numParticles = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the time resolution: ");
+        int timeResolution = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the simulation time: ");
+        double simulationTime = double.Parse(Console.ReadLine());
+
+        // Define the quantum system and initial conditions
+        Complex[] initialState = new Complex[numParticles];
+        for (int i = 0; i < numParticles; i++)
+        {
+            initialState[i] = Complex.One;
+        }
+
+        // Simulate the time evolution of the quantum system
+        double deltaTime = simulationTime / timeResolution;
+        Complex[][] timeWavefunctions = new Complex[timeResolution][];
+
+        for (int t = 0; t < timeResolution; t++)
+        {
+            timeWavefunctions[t] = new Complex[numParticles];
+            double time = t * deltaTime;
+
+            // Calculate the time-dependent wavefunction for each particle
+            for (int particle = 0; particle < numParticles; particle++)
+            {
+                double phase = 2 * Math.PI * time; // Simple linear time evolution
+                timeWavefunctions[t][particle] = Complex.Exp(Complex.ImaginaryOne * phase) * initialState[particle];
+            }
+        }
+
+        // Visualize the time wavefunctions
+        for (int t = 0; t < timeResolution; t++)
+        {
+            double currentTime = t * deltaTime;
+            Console.WriteLine($"Time: {currentTime}");
+
+            for (int particle = 0; particle < numParticles; particle++)
+            {
+                Console.WriteLine($"Particle {particle + 1}: {timeWavefunctions[t][particle]}");
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey(); // Wait for a key press before closing the console window
+    }
+}
